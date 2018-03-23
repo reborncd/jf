1771b584338bb10645a5d496001df1476f64195e
zwzjyxf <2682816847@qq.com>
2018-03-23 18:40:37 +0800

    修改package整合

--- src/main.js
+++ src/main.js
@@ -196,8 +196,8 @@ Vue.prototype.$format = (time) => {
 let instance = axios.create({
     //baseURL: "http://172.16.3.95:8080/JiFu_Project",//薛
     //baseURL: "http://172.16.1.200:8080/JiFu_Project",//安
-    //baseURL:"http://172.16.2.124:8082",//欧
-    baseURL: "http://172.16.2.8:8989/JiFu_Project",//康
+    baseURL:"http://172.16.2.124:8082",//欧
+    // baseURL: "http://172.16.2.8:8989/JiFu_Project",//康
     //baseURL:"http://192.168.1.106:8080",
     //baseURL:"http://192.168.1.179:8082",
     //baseURL:"http://127.0.0.1:8082",
